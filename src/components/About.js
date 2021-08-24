08e52b8962ee02b15fc106e4bfe893fa361e3756
Sheshagiri Kulkarni <girishak16@gmail.com>
2021-08-24 19:08:46 +0530

    responsive

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About(props) {
     return (
-        <div className={`container my-5 mx-2 text-${props.mode==='light'?'dark':'light'}`}>
+        <div className={`container my-5 text-${props.mode==='light'?'dark':'light'}`}>
             <h1>About Us</h1>
             <p className="my-3"><h3>
                 Welcome to Text Utilities, your source for all Text-Manipulations.<br /> We're interested to provide you simple text tools for free.
